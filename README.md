62fa091c279506f8c7878f994f34615576ffeda5
Richard Apodaca <rich.apodaca@gmail.com>
2022-08-08 15:37:00 -0700

    Installs test harness, updates test strings.

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
-# InChI EBNF
+# InChI Grammar
 
-Documentation of InChI syntax as a formal grammar using EBNF.
+Documentation of InChI syntax as a formal grammar in [W3C EBNF notation](https://www.w3.org/TR/REC-xml/#sec-notation). This work in progress currently recognizes the prefix, formula (partial), connections, and h\_atoms layers.
+
+# Run Tests
+
+The test suite is written in Ruby and uses the [EBNF gem](https://github.com/dryruby/ebnf). [test.txt](text.txt) contains the test strings. To install the harness and run the tests:
+
+```bash
+cd test
+bundle install --path vendor/bundle
+ruby test.rb
+```
---
+++ test/test.rb
@@ -0,0 +1,31 @@
+require 'rubygems'
+require 'bundler/setup'
+require 'ebnf'
+require 'ebnf/terminals'
+require 'ebnf/peg/parser'
+
+class Harness
+    include EBNF::PEG::Parser
+
+    def initialize
+        ebnf = File.open(File.expand_path("../../grammar.ebnf", __FILE__))
+
+        @rules = EBNF.parse(ebnf).make_peg.ast
+    end
+
+    def test(input)
+        begin
+            parse(input, :String, @rules)
+            nil
+        rescue EBNF::PEG::Parser::Error => e
+            input + ":" + e.message
+        end
+    end
+end
+
+File.readlines('../test.txt').each do |line|
+    c = Harness::new();
+    result = c.test(line)
+
+    puts result if !result.nil?
+end
